b0f293f5a2609d627ec4bacad530bbdd806ffefb
wedodesign <gabriieelreeis@gmail.com>
2013-12-31 14:57:15 -0200

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<script type="text/javascript">
+
+d = new Date();
+hour = d.getHours();
+if(hour < 24)
+{
+  document.write('<?php echo '2013'; ?>');
+}
+else
+{
+  document.write("Boa noite");
+}
+
+
+</SCRIPT>
+</head>
+</html>
